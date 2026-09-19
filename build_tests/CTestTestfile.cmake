3b883e30a2196d5c63c5781c06e34a953b53b77a
Jacky Mpoka <jackympoka22@gmail.com>
2026-09-19 04:33:10 +0200

    Added build_tests, build_arm_check_*/, CMakeCache.txt, and CMakeFiles/ to .gitignore.
    Removed all tracked generated build directories from Git’s index using git rm --cached.
    Local build folders were not deleted.
    Verified no generated build paths remain tracked.
    Fresh native CI configure/build/test passed:

--- build_tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/jacky/Documents/GitHub/Microcontroller-Drivers
-# Build directory: /mnt/c/Users/jacky/Documents/GitHub/Microcontroller-Drivers/build_tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[HostLogicTests]=] "/mnt/c/Users/jacky/Documents/GitHub/Microcontroller-Drivers/build_tests/host_tests")
-set_tests_properties([=[HostLogicTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/jacky/Documents/GitHub/Microcontroller-Drivers/CMakeLists.txt;47;add_test;/mnt/c/Users/jacky/Documents/GitHub/Microcontroller-Drivers/CMakeLists.txt;0;")
